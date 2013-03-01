42f2feca41c58bc4fd5235b4c71b050bd7c497b3
RoSk0 <roskoliy.kirill@gmail.com>
2013-03-01 19:50:21 +0200

    Applied Zels patch with path renaming(crm_core_paths_1.patch).

--- modules/crm_core_contact/contact.tpl.php
+++ modules/crm_core_contact/contact.tpl.php
@@ -4,7 +4,7 @@
 ?>
 <div id="contact-<?php print $type . '-' . $cid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if ($view_mode !== 'full'): ?>
-	  <h2<?php print $title_attributes; ?>><a href="<?php print base_path(); ?>crm/contact/<?php print $cid; ?>"><?php print render($contact_data['contact_name']); ?></a></h2>
+	  <h2<?php print $title_attributes; ?>><a href="<?php print base_path(); ?>crm-core/contact/<?php print $cid; ?>"><?php print render($contact_data['contact_name']); ?></a></h2>
   <?php endif; ?>
   <?php if ($view_mode === 'full'): ?>
 	  <?php print render($contact_data); ?>
