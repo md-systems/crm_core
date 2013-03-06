1447f586019e075d24a08977bedaf327fc28fb0b
Artem Shymko <itwebcross@gmail.com>
2013-03-06 20:02:35 +0000

    Issue #12175 - Correct templates for relationship

--- modules/crm_core_relationship/templates/relationship.tpl.php
+++ modules/crm_core_relationship/templates/relationship.tpl.php
@@ -2,14 +2,23 @@
 
 /**
  * @file
- * Generic relationship display template
+ * Theme implementation for relation entity.
+ *
+ * Available variables:
+ * - $content: An array of comment items. Use render($content) to print them all, or
+ *   print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_entity()
+ * @see template_process()
  */
 ?>
-<div id="relationship-<?php print $relationship->relation_type . '-' . $relationship->rid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php if ($view_mode !== 'full'): ?>
-
-  <?php endif; ?>
-  <?php if ($view_mode === 'full'): ?>
-
-  <?php endif; ?>
+<div class="relation relation-<?php print $relationship->relation_type; ?> clearfix"<?php print $attributes; ?>>
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      print render($content);
+    ?>
+  </div>
 </div>
