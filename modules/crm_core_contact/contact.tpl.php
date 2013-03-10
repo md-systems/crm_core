a6ade49d45e0499eb37ec12de5a1202601bde4ec
Michael Haggerty <mhaggerty@trellon.com>
2013-03-10 18:48:59 -0400

    by techsoldaten: adding file block to contact.tpl.php

--- modules/crm_core_contact/contact.tpl.php
+++ modules/crm_core_contact/contact.tpl.php
@@ -1,6 +1,18 @@
 <?php 
-  // contact.tpl.php
-  // generic contact display template
+/**
+ * @file
+ * Theme implementation for contact entity.
+ *
+ * Available variables:
+ * - $content: An array of comment items. Use render($content) to print them all, or
+ *   print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_entity()
+ * @see template_process()
+ */
 ?>
 <div id="contact-<?php print $type . '-' . $cid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if ($view_mode !== 'full'): ?>
