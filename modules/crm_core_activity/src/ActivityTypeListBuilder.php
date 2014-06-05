0501319c6b21144379297f7064b2243c5806b061
corvus_ch <christian.haeusler@md-systems.ch>
2014-06-05 14:22:49 +0200

    Add admin ui for activity types

---
+++ modules/crm_core_activity/src/ActivityTypeListBuilder.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\crm_core_activity\ActivityTypeListBuilder.
+ */
+
+namespace Drupal\crm_core_activity;
+
+use Drupal\Component\Utility\Xss;
+use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Class ActivityTypeListBuilder
+ *
+ * List builder for the activity type entity.
+ *
+ * @package Drupal\crm_core_activity
+ * @see \Drupal\crm_core_activity\Entity\ActivityType
+ */
+class ActivityTypeListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header = array();
+
+    $header['title'] = t('Name');
+
+    $header['description'] = array(
+      'data' => t('Description'),
+      'class' => array(RESPONSIVE_PRIORITY_MEDIUM),
+    );
+
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    $row = array();
+
+    $row['title'] = array(
+      'data' => $this->getLabel($entity),
+      'class' => array('menu-label'),
+    );
+
+    $row['description'] = Xss::filterAdmin($entity->description);
+
+    return $row + parent::buildRow($entity);
+  }
+}
