b2cdb2b8fa4458e08532133c8107a85d20debbf1
corvus_ch <christian.haeusler@md-systems.ch>
2014-06-07 10:22:38 +0200

    Make matching rule configuration a config entity

---
+++ modules/crm_core_default_matching_engine/src/Entity/MatchingRule.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\crm_core_default_matching_engine\Entity\MatchingRule.
+ */
+
+namespace Drupal\crm_core_default_matching_engine\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\crm_core_contact\Entity\ContactType;
+
+/**
+ * CRM default match engine rule Entity Class.
+ *
+ * @ConfigEntityType(
+ *   id = "crm_core_default_engine_rule",
+ *   label = @Translation("Matching Rule"),
+ *   config_prefix = "rule",
+ *   controllers = {
+ *     "form" = {
+ *       "default" = "Drupal\crm_core_default_matching_engine\Form\MatchingRuleForm",
+ *     },
+ *   },
+ *   entity_keys = {
+ *     "id" = "type",
+ *     "label" = "label",
+ *   },
+ *   links = {
+ *     "canonical" = "crm_core_default_matching_engine.rule_edit",
+ *     "add-form" = "crm_core_default_matching_engine.rule_add",
+ *     "edit-form" = "crm_core_default_matching_engine.rule_edit",
+ *   }
+ * )
+ */
+class MatchingRule extends ConfigEntityBase {
+
+  /**
+   * Primary identifier.
+   *
+   * Matches the id of a contact type.
+   *
+   * @var string
+   */
+  public $type;
+
+  /**
+   * Defines the score at which a contact is considered a match.
+   *
+   * @var int
+   */
+  public $threshold = 0;
+
+  /**
+   * The order in which to return contact matches.
+   *
+   * @var string
+   */
+  public $return_order = 'created';
+
+  /**
+   * Strict matching status.
+   *
+   * @var bool
+   */
+  public $strict = FALSE;
+
+  /**
+   * @var array
+   */
+  public $fields = array();
+
+  /**
+   * The entity label.
+   *
+   * @var string
+   */
+  protected $label;
+
+  /**
+   * Overrides Entity::id().
+   */
+  public function id() {
+    return $this->type;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function label() {
+    if (empty($this->label)) {
+      $type = ContactType::load($this->type);
+      $this->label = t('@type Matching Rule', array(
+        '@type' => $type->label(),
+      ));
+    }
+    return parent::label();
+  }
+
+}
