2476cc1092da1b717ca6b9c472af2cd676db40d5
Arild Matsson <arild@klavaro.se>
2015-02-26 17:57:20 +0100

    Converted matcher permissions and corrected schema

---
+++ modules/crm_core_default_matching_engine/src/Plugin/crm_core_match/field/String.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Contains
+ *   \Drupal\crm_core_default_matching_engine\Plugin\crm_core_match\field\String.
+ */
+
+namespace Drupal\crm_core_default_matching_engine\Plugin\crm_core_match\field;
+
+/**
+ * Class for evaluating string fields.
+ *
+ * @CrmCoreMatchFieldHandler (
+ *   id = "string"
+ * )
+ */
+class String extends Text {
+
+}
